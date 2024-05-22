64aa01cfab50169a6f8d763db3fa07262120944e
Sergey Lubkov <ser.lubkov@gmail.com>
2024-05-22 13:28:06 +0300

    add common level

---
+++ MapEditor/Source/Common/ME.DB.DAO.pas
@@ -0,0 +1,99 @@
+unit ME.DB.DAO;
+
+interface
+
+uses
+  System.Classes, System.SysUtils, System.Variants, Data.DB, MemDS, DBAccess, Uni,
+  ME.DB.Entity;
+
+type
+  TDAOClass = class of TDAOCommon;
+
+  TDAOCommon = class(TObject)
+  private
+    FConnection: TCustomConnection;
+
+    function GetConnection: TUniConnection;
+  protected
+    function EntityClass: TEntityClass; virtual; abstract;
+  public
+    constructor Create(const Connection: TCustomConnection);
+
+    function GetNewInstance: TEntity; virtual; //CreateInstance
+    function GetAt(ID: Integer; const Entity: TEntity): Boolean; virtual; abstract;
+    procedure Insert(const Entity: TEntity); virtual; abstract;
+    procedure Update(const Entity: TEntity); virtual; abstract;
+    procedure Remove(const ID: Variant);
+    procedure TruncateTable;
+    function RecordCount: Int64;
+
+    property Connection: TUniConnection read GetConnection;
+  end;
+
+implementation
+
+{ TDAOCommon }
+
+constructor TDAOCommon.Create(const Connection: TCustomConnection);
+begin
+  inherited Create;
+
+  FConnection := Connection;
+end;
+
+function TDAOCommon.GetConnection: TUniConnection;
+begin
+  Result := TUniConnection(FConnection);
+end;
+
+function TDAOCommon.GetNewInstance: TEntity;
+begin
+  Result := EntityClass.Create;
+end;
+
+procedure TDAOCommon.Remove(const ID: Variant);
+var
+  Query: TUniQuery;
+begin
+  Query := TUniQuery.Create(nil);
+  try
+    Query.Connection := Connection;
+    Query.SQL.Text := 'DELETE FROM ' + EntityClass.EntityName + ' WHERE ID = :ID';
+    Query.ParamByName('ID').Value := ID;
+    Query.Execute;
+  finally
+    Query.Free;
+  end;
+end;
+
+procedure TDAOCommon.TruncateTable;
+var
+  Query: TUniQuery;
+begin
+  Query := TUniQuery.Create(nil);
+  try
+    Query.Connection := Connection;
+    Query.SQL.Text := 'DELETE FROM ' + EntityClass.EntityName;
+    Query.Execute;
+  finally
+    Query.Free;
+  end;
+end;
+
+function TDAOCommon.RecordCount: Int64;
+var
+  Query: TUniQuery;
+begin
+  Query := TUniQuery.Create(nil);
+  try
+    Query.Connection := Connection;
+    Query.SQL.Text := 'SELECT Count(*) RecCount FROM ' + EntityClass.EntityName;
+    Query.Open;
+
+    Result := Query.Fields[0].AsLargeInt;
+  finally
+    Query.Free;
+  end;
+end;
+
+end.
---
+++ MapEditor/Source/Common/ME.Edit.Form.Presenter.pas
@@ -0,0 +1,85 @@
+﻿unit ME.Edit.Form.Presenter;
+
+interface
+
+uses
+  System.SysUtils, System.Variants, System.Classes, Vcl.Controls, Vcl.Forms,
+  ME.Dialog.Presenter;
+
+type
+  IEditDialog<T> = interface(ICLDialog)
+    ['{7B740715-319C-4285-84FE-D16EF81F7B40}']
+
+    procedure SetInstance(const Value: T);
+    procedure PostValues(const Value: T);
+  end;
+
+  TEditFormPresenter<T> = class(TDialogPresenter)
+  private
+    FInstance: T;
+  protected
+    procedure SetInstance(const Value: T); virtual;
+    procedure PostValues; virtual;
+    procedure InternalSave; virtual; abstract;
+    procedure Cancel; virtual; abstract;
+    function Validate(var vMessage: string): Boolean; virtual;
+  public
+    constructor Create(Dialog: IEditDialog<T>; Instance: T); overload; virtual;
+
+    function Edit: Boolean;
+
+    property Instance: T read FInstance write SetInstance;
+  end;
+
+implementation
+
+{ TEditFormPresenter<T> }
+
+constructor TEditFormPresenter<T>.Create(Dialog: IEditDialog<T>; Instance: T);
+begin
+  inherited Create(Dialog);
+
+  Self.Instance := Instance;
+end;
+
+procedure TEditFormPresenter<T>.SetInstance(const Value: T);
+begin
+  FInstance := Value;
+  IEditDialog<T>(FEditDialog).SetInstance(Value);
+end;
+
+procedure TEditFormPresenter<T>.PostValues;
+begin
+  IEditDialog<T>(FEditDialog).PostValues(FInstance);
+end;
+
+function TEditFormPresenter<T>.Validate(var vMessage: string): Boolean;
+begin
+  Result := True;
+  vMessage := '';
+end;
+
+function TEditFormPresenter<T>.Edit: Boolean;
+var
+  ErrorText: string;
+begin
+  Result := ShowModal;
+  if not Result then begin
+    Cancel;
+    Exit;
+  end;
+
+  if not Validate(ErrorText) then
+  begin
+    if Assigned(OnError) then
+      OnError(ErrorText);
+
+    FEditDialog.SetModalResult(mrNone);
+    Exit;
+  end;
+
+  PostValues;
+  InternalSave;
+end;
+
+end.
