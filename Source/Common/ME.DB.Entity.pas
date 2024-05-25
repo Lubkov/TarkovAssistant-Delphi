5564dd3313f83a178cb37124766d6d7869ed9fcb
Sergey Lubkov <ser.lubkov@gmail.com>
2024-05-25 16:29:00 +0300

    store picture

--- Source/Common/ME.DB.Entity.pas
+++ Source/Common/ME.DB.Entity.pas
@@ -3,7 +3,8 @@ unit ME.DB.Entity;
 interface
 
 uses
-  System.Classes, System.SysUtils, System.Variants, Data.DB;
+  System.Classes, System.SysUtils, System.Variants, FMX.Graphics, Data.DB,
+  Uni;
 
 type
   TEntityClass = class of TEntity;
@@ -19,6 +20,9 @@ type
 
     procedure Assign(const Source: TEntity); overload; virtual;
     procedure Assign(const DataSet: TDataSet); overload; virtual;
+    class procedure AssignPicture(const Field: TField; Bitmap: TBitmap);
+    class procedure AssignPictureTo(const Source: TBitmap; const Field: TField); overload;
+    class procedure AssignPictureTo(const Source: TBitmap; Param: TParam); overload;
 
     class function EntityName: string; virtual; abstract;
     class function FieldList: string; virtual; abstract;
@@ -57,6 +61,69 @@ end;
 procedure TEntity.Assign(const DataSet: TDataSet);
 begin
   FID := DataSet.FieldByName('ID').Value;
+end;
+
+class procedure TEntity.AssignPicture(const Field: TField; Bitmap: TBitmap);
+var
+  Stream: TMemoryStream;
+  EmptyPicture: TBitmap;
+begin
+  if Field.IsNull then begin
+    EmptyPicture := TBitmap.Create;
+    try
+      Bitmap. Assign(EmptyPicture);
+    finally
+      EmptyPicture.Free;
+    end;
+
+    Exit;
+  end;
+
+  Stream := TMemoryStream.Create;
+  try
+    TBlobField(Field).SaveToStream(Stream);
+    Stream.Position := 0;
+    if Stream.Size > 0 then
+      Bitmap.LoadFromStream(Stream)
+    else
+      Bitmap.Assign(nil);
+  finally
+    Stream.Free;
+  end;
+end;
+
+class procedure TEntity.AssignPictureTo(const Source: TBitmap; const Field: TField);
+var
+  Stream: TMemoryStream;
+begin
+  Stream := TMemoryStream.Create;
+  try
+    Source.SaveToStream(Stream);
+    Stream.Position := 0;
+    TBlobField(Field).LoadFromStream(Stream);
+  finally
+    Stream.Free;
+  end;
+end;
+
+class procedure TEntity.AssignPictureTo(const Source: TBitmap; Param: TParam);
+var
+  Stream: TMemoryStream;
+begin
+ if Source.IsEmpty then begin
+   Param.Value := Null;
+   Exit;
+ end;
+
+  Stream := TMemoryStream.Create;
+  try
+    Source.SaveToStream(Stream);
+    Stream.Position := 0;
+
+    TUniParam(Param).LoadFromStream(Stream, ftBlob);
+  finally
+    Stream.Free;
+  end;
 end;
 
 end.
