b4b28eec2b0f9f8b0fa8383d348f203aafc85ea1
Sergey Lubkov <ser.lubkov@gmail.com>
2024-06-03 21:02:25 +0300

    add background color

--- Source/GUI/Form/TM.MainForm.pas
+++ Source/GUI/Form/TM.MainForm.pas
@@ -6,7 +6,7 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
   TM.Form.Wrapper, FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts,
-  System.ImageList, FMX.ImgList, FMX.Objects;
+  System.ImageList, FMX.ImgList, FMX.Objects, System.Actions, FMX.ActnList;
 
 type
   TMainForm = class(TForm)
@@ -19,6 +19,7 @@ type
     buZoomOut: TSpeedButton;
     buCentreMap: TSpeedButton;
     buMapFilters: TSpeedButton;
+    ActionList1: TActionList;
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
   private
@@ -34,7 +35,12 @@ implementation
 {$R *.fmx}
 
 procedure TMainForm.FormCreate(Sender: TObject);
+const
+  BackgroundColor = $FF0F0F0F;
 begin
+  Self.Fill.Color := BackgroundColor;
+  Self.Fill.Kind := TBrushKind.Solid;
+
   FFormWrapper := TFormWrapper.Create(Self);
 end;
 
