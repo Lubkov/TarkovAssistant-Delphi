26bcd6750815c2aae153109b244852b53c8c9b43
SergeyL <SergeyL@NB131.datasoft.local>
2024-06-20 11:49:56 +0300

    App.Service, remove db level

--- Source/Common/App.Service.pas
+++ Source/Common/App.Service.pas
@@ -3,30 +3,17 @@ unit App.Service;
 interface
 
 uses
-  System.SysUtils, System.Variants, System.Classes,
-  App.SQLite.Connection;
+  System.SysUtils, System.Variants, System.Classes;
 
 type
   TAppService = class(TComponent)
   private
-    FConnection: TSQLiteConnection;
-
-    function GetConnected: Boolean;
-    procedure SetConnected(const Value: Boolean);
-    function GetDatabase: string;
   public
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
 
-    procedure Connect;
-    procedure Disconnect;
-
     procedure LoadParams;
     procedure LoadDataFromJSON;
-
-    property Connected: Boolean read GetConnected write SetConnected;
-    property Connection: TSQLiteConnection read FConnection;
-    property Database: string read GetDatabase;
   end;
 
 var
@@ -35,8 +22,7 @@ var
 implementation
 
 uses
-  App.Constants, Map.Data.Service, ME.Service.Map, ME.Service.Layer,
-  ME.Service.Marker, ME.Service.Quest;
+  App.Constants, Map.Data.Service;
 
 { TAppService }
 
@@ -45,55 +31,18 @@ begin
   inherited;
 
   DataSertvice := TDataSertvice.Create;
-  FConnection := TSQLiteConnection.Create(Self);
-  MapService := TMapService.Create(FConnection.Connection);
-  LayerService := TLayerService.Create(FConnection.Connection);
-  MarkerService := TMarkerService.Create(FConnection.Connection);
-  QuestService := TQuestService.Create(FConnection.Connection);
 end;
 
 destructor TAppService.Destroy;
 begin
   DataSertvice.Free;
-  FreeAndNil(MapService);
-  FreeAndNil(LayerService);
-  FreeAndNil(MarkerService);
-  FreeAndNil(QuestService);
-
-  FreeAndNil(FConnection);
 
   inherited;
-end;
-
-function TAppService.GetConnected: Boolean;
-begin
-  Result := FConnection.Connected;
-end;
-
-procedure TAppService.SetConnected(const Value: Boolean);
-begin
-  FConnection.Connected := Value;
-end;
-
-function TAppService.GetDatabase: string;
-begin
-  Result := FConnection.Connection.Database;
-end;
-
-procedure TAppService.Connect;
-begin
-  FConnection.Connect;
-end;
-
-procedure TAppService.Disconnect;
-begin
-  FConnection.Disconnect;
 end;
 
 procedure TAppService.LoadParams;
 begin
   AppParams.Load;
-//  FConnection.Database := AppParams.DatabasePath;
 end;
 
 procedure TAppService.LoadDataFromJSON;
