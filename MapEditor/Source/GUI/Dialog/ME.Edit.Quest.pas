684376c52ec06c4e1cc6f4552556f7e77c2c8195
Sergey Lubkov <sergeyl@devart.com>
2025-04-07 17:15:35 +0300

    added filter for the traders

--- MapEditor/Source/GUI/Dialog/ME.Edit.Quest.pas
+++ MapEditor/Source/GUI/Dialog/ME.Edit.Quest.pas
@@ -6,17 +6,17 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
   System.Actions, FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs,
   FMX.StdCtrls, FMX.ActnList, FMX.Controls.Presentation, FMX.Edit, FMX.ListBox,
-  FMX.Layouts, ME.Edit.Form, ME.Edit.Form.Presenter, ME.DB.Quest;
+  FMX.Layouts, ME.Edit.Form, ME.Edit.Form.Presenter, ME.DB.Quest, ME.Filter.Trader;
 
 type
   TedQuest = class(TEditForm, IEditDialog<TDBQuest>)
     MainLayout: TLayout;
     laQuestName: TLabel;
     edQuestName: TEdit;
-    laTraderName: TLabel;
-    edTraderName: TComboBox;
+    TraderLayout: TLayout;
   private
     FQuest: TDBQuest;
+    FTraderEdit: TTraderFilter;
 
     function GetQuestName: string;
     procedure SetQuestName(const Value: string);
@@ -40,16 +40,15 @@ implementation
 { TedQuest }
 
 constructor TedQuest.Create(AOwner: TComponent);
-var
-  Trader: TTrader;
 begin
   inherited;
 
   FQuest := nil;
 
-  edTraderName.Clear;
-  for Trader := TTrader.Prapor to TTrader.Lightkeeper do
-    edTraderName.Items.Add(TDBQuest.TraderToStr(Trader));
+  FTraderEdit := TTraderFilter.Create(Self);
+  FTraderEdit.Parent := TraderLayout;
+  FTraderEdit.Align := TAlignLayout.Client;
+  FTraderEdit.ClearActionVisible := False;
 end;
 
 destructor TedQuest.Destroy;
@@ -71,15 +70,12 @@ end;
 
 function TedQuest.GetTraderName: TTrader;
 begin
-  if edTraderName.ItemIndex >= 0 then
-    Result := TTrader(edTraderName.ItemIndex + 1)
-  else
-    Result := TTrader.None;
+  Result := FTraderEdit.TraderName;
 end;
 
 procedure TedQuest.SetTraderName(const Value: TTrader);
 begin
-  edTraderName.ItemIndex := Ord(Value) - 1;
+  FTraderEdit.TraderName := Value;
 end;
 
 procedure TedQuest.SetInstance(const Value: TDBQuest);
