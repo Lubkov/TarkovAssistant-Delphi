83b1f86fe0c47d4ca5f789a15bb8877c5df3e5eb
Sergey Lubkov <sergeyl@devart.com>
2025-04-08 15:33:49 +0300

    Trader filter refactoring

--- MapEditor/Source/GUI/Filter/ME.Filter.Trader.pas
+++ MapEditor/Source/GUI/Filter/ME.Filter.Trader.pas
@@ -3,38 +3,27 @@ unit ME.Filter.Trader;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  System.Actions, System.ImageList, FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms,
-  FMX.Dialogs, FMX.StdCtrls, FMX.ListBox, FMX.Controls.Presentation, FMX.Layouts,
-  FMX.ImgList, FMX.ActnList, ME.DB.Quest, ME.Trader;
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
+  FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
+  ME.Form.Filter, System.Actions, FMX.ActnList, System.ImageList, FMX.ImgList,
+  FMX.Controls.Presentation, FMX.Layouts, FMX.ListBox, ME.Trader;
 
 type
-  TTraderFilter = class(TFrame)
+  TTraderFilter = class(TFormFilter)
     laTraderName: TLabel;
     edTraderName: TComboBox;
-    MainLayout: TLayout;
-    ToolLayout: TLayout;
-    ButtonLayout: TLayout;
-    edEditMap: TSpeedButton;
-    ImageList1: TImageList;
-    ActionList1: TActionList;
-    acClearFilter: TAction;
-    procedure acClearFilterExecute(Sender: TObject);
+
     procedure edTraderNameChange(Sender: TObject);
   private
-    FOnChange: TNotifyEvent;
-
-    function GetTraderName: TTrader;
-    procedure SetTraderName(const Value: TTrader);
-    function GetClearActionVisible: Boolean;
-    procedure SetClearActionVisible(const Value: Boolean);
+    function GetTrader: TTrader;
+    procedure SetTrader(const Value: TTrader);
+  protected
+    function GetKeyValue: Variant; override;
+    procedure SetKeyValue(const Value: Variant); override;
   public
-    constructor Create(AOwner: TComponent); override;
-    destructor Destroy; override;
+    procedure Init; override;
 
-    property TraderName: TTrader read GetTraderName write SetTraderName;
-    property ClearActionVisible: Boolean read GetClearActionVisible write SetClearActionVisible;
-    property OnChange: TNotifyEvent read FOnChange write FOnChange;
+    property Trader: TTrader read GetTrader write SetTrader;
   end;
 
 implementation
@@ -43,59 +32,49 @@ implementation
 
 { TTraderFilter }
 
-constructor TTraderFilter.Create(AOwner: TComponent);
+procedure TTraderFilter.edTraderNameChange(Sender: TObject);
+begin
+  DoChange;
+end;
+
+function TTraderFilter.GetTrader: TTrader;
+begin
+  if KeyValue = Null then
+    Result := TTrader.None
+  else
+    Result := TTrader(KeyValue + 1);
+end;
+
+procedure TTraderFilter.SetTrader(const Value: TTrader);
+begin
+  KeyValue := Ord(Value) - 1;
+end;
+
+function TTraderFilter.GetKeyValue: Variant;
+begin
+  if edTraderName.ItemIndex >= 0 then
+    Result := edTraderName.ItemIndex + 1
+  else
+    Result := Null;
+end;
+
+procedure TTraderFilter.SetKeyValue(const Value: Variant);
+begin
+  if VarIsEmpty(Value) or VarIsNull(Value) then
+    edTraderName.ItemIndex := -1
+  else
+    edTraderName.ItemIndex := Ord(Integer(Value)) - 1;
+end;
+
+procedure TTraderFilter.Init;
 var
   Trader: TTrader;
 begin
   inherited;
 
-  FOnChange := nil;
-
   edTraderName.Clear;
   for Trader := TTrader.Prapor to TTrader.Lightkeeper do
     edTraderName.Items.Add(TraderToStr(Trader));
-end;
-
-destructor TTraderFilter.Destroy;
-begin
-  FOnChange := nil;
-
-  inherited;
-end;
-
-function TTraderFilter.GetTraderName: TTrader;
-begin
-  if edTraderName.ItemIndex >= 0 then
-    Result := TTrader(edTraderName.ItemIndex + 1)
-  else
-    Result := TTrader.None;
-end;
-
-procedure TTraderFilter.SetTraderName(const Value: TTrader);
-begin
-  edTraderName.ItemIndex := Ord(Value) - 1;
-end;
-
-function TTraderFilter.GetClearActionVisible: Boolean;
-begin
-  Result := acClearFilter.Visible;
-end;
-
-procedure TTraderFilter.edTraderNameChange(Sender: TObject);
-begin
-  if Assigned(FOnChange) then
-    FOnChange(Self);
-end;
-
-procedure TTraderFilter.SetClearActionVisible(const Value: Boolean);
-begin
-  acClearFilter.Visible := Value;
-  ToolLayout.Visible := Value;
-end;
-
-procedure TTraderFilter.acClearFilterExecute(Sender: TObject);
-begin
-  edTraderName.ItemIndex := -1;
 end;
 
 end.
